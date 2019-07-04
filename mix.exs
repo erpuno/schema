b8b1897204fbf7bc52632195ad2f36ff9a738982
Namdak Tonpa <maxim@synrc.com>
2019-07-04 13:48:42 +0300

    0.7.3

---
+++ mix.exs
@@ -0,0 +1,36 @@
+defmodule ERP.Mixfile do
+  use Mix.Project
+
+  def project() do
+    [
+      app: :erp,
+      version: "0.7.3",
+      elixir: "~> 1.7",
+      description: "ERP Enterprise Resource Planning",
+      package: package(),
+      deps: deps()
+    ]
+  end
+
+  def package do
+    [
+      files: ~w(doc lib include src mix.exs LICENSE),
+      licenses: ["ISC"],
+      maintainers: ["Namdak Tonpa"],
+      name: :erp,
+      links: %{"GitHub" => "https://github.com/enterprizing/erp"}
+    ]
+  end
+
+  def application() do
+    [mod: {:erp, []}]
+  end
+
+  def deps() do
+    [
+      {:kvs, "~> 6.7.3"},
+      {:rocksdb, "~> 1.2.0"},
+      {:ex_doc, "~> 0.11", only: :dev}
+    ]
+  end
+end
