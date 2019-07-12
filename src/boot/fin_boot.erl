c4c3c9ec8945cdbc6de9f71a7763cd05276218b2
Namdak Tonpa <maxim@synrc.com>
2019-07-12 04:12:18 +0300

    wip

--- src/boot/fin_boot.erl
+++ src/boot/fin_boot.erl
@@ -26,7 +26,12 @@ acc("CATALX" = X ) ->
      #'Acc'{id = X ++ "/R&D", rate = {2,15}}
    ].
 
-payments(C) -> kvs:all("/plm/"++C++"/income").
+profit(C) ->
+  lists:map(fun({#'Payment'{volume=Vo1,price=Pr1} = P1,
+                 #'Payment'{volume=Vo2,price=Pr2} = P2}) ->
+               P1#'Payment'{volume={0,1},price=dec:sub(dec:mul(Vo1,Pr1),dec:mul(Vo2,Pr2))}
+        end, lists:zip(kvs:all("/plm/"++C++"/income"),
+                       kvs:all("/plm/"++C++"/outcome"))).
 
 rate(#'Payment'{price=P, volume=V}=Pay,#'Acc'{id=Id, rate=R}=Acc,C) ->
   Pay#'Payment'{invoice= kvs:seq([],[]), volume={0,1}, price=dec:mul(R,dec:mul(P,V))}.
@@ -39,7 +44,7 @@ accounts() ->
       Feed = lists:concat(["/fin/tx/",Id]),
       case kvs:get(writer, Feed) of
            {error,_} -> lists:map(fun(#'Payment'{}=Pay) ->
-                        kvs:append(rate(Pay,SubAcc,C), Feed) end, payments(C));
+                        kvs:append(rate(Pay,SubAcc,C), Feed) end, profit(C));
              {ok,_} -> skip
       end
     end, acc(C))
@@ -49,14 +54,14 @@ inv_boot() ->
    lists:map(fun(#'Product'{code=C}) ->
       {ok, #'Acc'{rate= Rate}=Acc} = kvs:get("/fin/acc/" ++ C, C ++ "/options"),
       Hours = lists:foldl(fun (#'Person'{hours=A},Acc) -> Acc + A end,0,kvs:all("/plm/"++C++"/staff")),
-      Feed = "/plm/"++C++"/investments",
-      case kvs:get(writer,Feed) of
-           {error,_} -> lists:map(fun(#'Person'{cn=Person,hours=X}) ->
-                        lists:map(fun(#'Payment'{}=Pay) ->
-                        Div = dec:'div'({0,X},{0,Hours}),
-                        NewPay = rate(Pay,Acc#'Acc'{rate = dec:mul(Rate,Div)},C),
-                        io:format("Pay ~p ~p/~p = ~p~n",[Person,Hours,X,NewPay]),
-                        kvs:append(NewPay,"/fin/iban/" ++ Person) end,
-                        kvs:all("/fin/tx/"++C++"/options")) end,
-                        kvs:all("/plm/"++C++"/staff"));
-              {ok,_} -> skip end  end, plm_boot:products()).
+      lists:map(fun(#'Person'{cn=Person,hours=X}) ->
+         Feed = "/fin/iban/" ++ Person,
+         case kvs:get(writer,Feed) of
+              {error,_} ->
+                  lists:map(fun(#'Payment'{}=Pay) ->
+                     Div = dec:'div'({0,X},{0,Hours}),
+                     NewPay = rate(Pay,Acc#'Acc'{rate = dec:mul(Rate,Div)},C),
+                     kvs:append(NewPay,Feed) end, kvs:all("/fin/tx/"++C++"/options"));
+              {ok,_} -> skip
+         end end, kvs:all("/plm/"++C++"/staff"))
+   end, plm_boot:products()).
