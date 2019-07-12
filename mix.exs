9e9f9aa0e30cabd6fa0b4e3a8bd3db80571e17d2
Namdak Tonpa <maxim@synrc.com>
2019-07-12 03:51:51 +0300

    cd /Users/maxim/depot/synrc/apps/erlang/erp/src

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ERP.Mixfile do
   def project() do
     [
       app: :erp,
-      version: "0.7.9",
+      version: "0.7.10",
       elixir: "~> 1.7",
       description: "ERP Enterprise Resource Planning",
       package: package(),
