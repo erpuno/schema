fc626a9b50622bca4827868e381404a1d9067e40
Namdak Tonpa <maxim@synrc.com>
2019-07-13 22:15:33 +0300

    new code from pal

--- src/boot/erp_boot.erl
+++ src/boot/erp_boot.erl
@@ -1,32 +1,21 @@
 -module(erp_boot).
 -compile(export_all).
 -include("organization.hrl").
--include("person.hrl").
--include("employee.hrl").
--include("buyer.hrl").
--include("investment.hrl").
--include("salary.hrl").
--include("payment.hrl").
--include("transport.hrl").
--include("seller.hrl").
--include("warehouse.hrl").
--include("inventory.hrl").
--include("material.hrl").
--include("contract.hrl").
 -include("location.hrl").
+-include("person.hrl").
 -include("product.hrl").
 -include("branch.hrl").
--include("delivery.hrl").
 -include_lib("kvs/include/kvs.hrl").
 -include_lib("kvs/include/metainfo.hrl").
 -include_lib("kvs/include/cursors.hrl").
 
 boot() ->
-    Structure    = [ "/erp/group", "/erp/partners", "/erp/quanterall" ],
     GroupOrgs    = [ #'Organization'{name="Quanterall", url="quanterall.com"} ],
+
     HeadBranches = [ #'Branch'{ loc = #'Loc'{ city = "Varna",   country = "BG" } },
                      #'Branch'{ loc = #'Loc'{ city = "Sophia",  country = "BG" } },
                      #'Branch'{ loc = #'Loc'{ city = "Plovdiv", country = "BG" } } ],
+
     PartnersOrgs = [ #'Organization'{name="NYNJA, Inc.",    url= "nynja.io"},
                      #'Organization'{name="Catalx Exchange Inc.", url="catalx.io"},
                      #'Organization'{name="FiaTech",        url=[]},
@@ -34,11 +23,16 @@ boot() ->
                      #'Organization'{name="SwissEMX",       url=[]},
                      #'Organization'{name="HistoricalPark", url=[]},
                      #'Organization'{name="Intralinks",     url=[]} ],
-    case {kvs:get(writer,"/erp/group"),kvs:get(writer,"/erp/partners")} of
-         {{error,_},{error,_}} ->
-              lists:map(fun(X) -> kvs:writer(X) end, Structure),
-              lists:map(fun(X) -> kvs:append(X,"/erp/quanterall") end, HeadBranches),
-              lists:map(fun(X) -> kvs:append(X,"/erp/group") end, GroupOrgs),
-              lists:map(fun(X) -> kvs:append(X,"/erp/partners") end, PartnersOrgs);
-                             _ -> skip end.
+
+    Structure    = [ {"/erp/group", GroupOrgs},
+                     {"/erp/partners", PartnersOrgs},
+                     {"/erp/quanterall", HeadBranches}],
+
+    lists:foreach(fun({Feed, Data}) ->
+                         case kvs:get(writer, Feed) of
+                              {error,_} -> kvs:writer(Feed),
+                                           lists:map(fun(X) -> kvs:append(X,Feed) end, Data);
+                                      _ -> skip
+                         end
+                  end, Structure).
 
