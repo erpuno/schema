91988a4ead1f798c58a6ad63c72d4a1032a27657
Namdak Tonpa <maxim@synrc.com>
2019-07-14 18:50:58 +0300

    helpful structure change, the final right one :-)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ERP.Mixfile do
   def project() do
     [
       app: :erp,
-      version: "0.7.12",
+      version: "0.7.13",
       elixir: "~> 1.7",
       description: "ERP Enterprise Resource Planning",
       package: package(),
--- src/boot/fin_boot.erl
+++ src/boot/fin_boot.erl
@@ -24,6 +24,12 @@ acc("CATALX" = X ) ->
      #'Acc'{id = X ++ "/options", rate = {2,10}},
      #'Acc'{id = X ++ "/reserved", rate = {2,15}},
      #'Acc'{id = X ++ "/R&D", rate = {2,15}}
+   ];
+
+acc(X) ->
+   [
+     #'Acc'{id = X ++ "/local", rate = {0,0}},
+     #'Acc'{id = X ++ "/bpe", rate = {0,0}}
    ].
 
 profit(C) ->
@@ -52,18 +58,17 @@ accounts() ->
 
 inv_boot() ->
    lists:map(fun(#'Product'{code=C}) ->
+      Staff = kvs:all("/plm/"++C++"/staff"),
       {ok, #'Acc'{rate= Rate}=Acc} = kvs:get("/fin/acc/" ++ C, C ++ "/options"),
-      Hours = lists:foldl(fun (#'Person'{hours=A},Acc) -> Acc + A end,0,kvs:all("/plm/"++C++"/staff")),
-      io:format("Product: ~p Hours: ~p~n",[C,Hours]),
+      Hours = lists:foldl(fun (#'Person'{hours=A},Acc) -> Acc + A end,0,Staff),
       lists:map(fun(#'Person'{cn=Person,hours=X}) ->
-         Feed = "/fin/iban/" ++ Person,
+         Feed = "/fin/tx/" ++ Person ++ "/local",
          case kvs:get(writer,Feed) of
               {error,_} ->
                   lists:map(fun(#'Payment'{}=Pay) ->
                      Div = dec:'div'({0,X},{0,Hours}),
                      NewPay = rate(Pay,Acc#'Acc'{rate = dec:mul(Rate,Div)},C),
-                     io:format("Person: ~p X: ~p Div: ~p Rate: ~p ~n",[Person,X,Div,Rate]),
                      kvs:append(NewPay,Feed) end, kvs:all("/fin/tx/"++C++"/options"));
               {ok,_} -> skip
-         end end, kvs:all("/plm/"++C++"/staff"))
+         end end, Staff)
    end, plm_boot:products()).
