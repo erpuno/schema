d6aaf099b1e8dd7dbfa03cc49ee6656b440f31cb
Namdak Tonpa <maxim@synrc.com>
2019-07-24 16:45:58 +0300

    0.7.17

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ERP.Mixfile do
   def project() do
     [
       app: :erp,
-      version: "0.7.16",
+      version: "0.7.17",
       elixir: "~> 1.7",
       description: "ERP Enterprise Resource Planning",
       package: package(),
