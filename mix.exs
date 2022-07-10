03f261d9121ee1166077f6692465c6c7f4ccc894
5HT <maxim@synrc.com>
2022-07-10 05:57:35 +0300

    ver

--- mix.exs
+++ mix.exs
@@ -4,9 +4,9 @@ defmodule ERP.Mixfile do
   def project() do
     [
       app: :schema,
-      version: "2.11.0",
+      version: "3.7.0",
       elixir: "~> 1.7",
-      description: "ERP Directory Schema",
+      description: "ERP/1 Directory Schema",
       package: package(),
       deps: deps()
     ]
@@ -29,9 +29,8 @@ defmodule ERP.Mixfile do
   def deps() do
     [
       {:dec, "~> 0.10.2"},
-      {:kvs, "~> 8.10.4"},
-      {:rocksdb, "~> 1.6.0"},
-      {:ex_doc, "~> 0.11", only: :dev}
+      {:kvs, "~> 9.4.8"},
+      {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
 end
